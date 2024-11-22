b690eb15cdbc4f6ab7dc554e7c4223e49dd53a2d
elizz14 <eliza.niculae2005@gmail.com>
2024-11-22 13:00:47 +0200

    Add Bubble Sort skeleton

---
+++ bubble-sort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+static void sort(void)
+{
+	int array[n];
+	for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1])
+                swap(arr, j, j + 1);
+        }
+   }
+}
+ 
+int main()
+{
+    return 0;
+}
